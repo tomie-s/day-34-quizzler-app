27c7c2d24a2d5c048a65b02ec65ea1c1ab60fdb0
Tomi Salami <salami.tomie@gmail.com>
2024-06-25 15:57:33 -0400

    Fixed bug related to user score display

--- ui.py
+++ ui.py
@@ -42,8 +42,8 @@ class QuizInterface:
 
     def get_next_question(self):
         self.canvas.config(bg='white')
+        self.score_label.configure(text=f"Score: {self.quiz.score}")
         if self.quiz.still_has_questions():
-            self.score_label.configure(text=f"Score: {self.quiz.score}")
             q_text = self.quiz.next_question()
             self.canvas.itemconfig(self.question_text, text=q_text, fill=THEME_COLOR)
         else:
